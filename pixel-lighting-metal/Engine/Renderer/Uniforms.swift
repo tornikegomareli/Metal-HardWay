03c3bce12015f24a9e497685e53ffc4bd309b1e3
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-01-07 17:05:39 +0400

    uniforms data model for pixel lighting

---
+++ pixel-lighting-metal/Engine/Renderer/Uniforms.swift
@@ -0,0 +1,21 @@
+//
+//  Uniforms.swift
+//  pixel-lighting-metal
+//
+//  Created by Tornike Gomareli on 07.01.25.
+//
+
+import simd
+
+struct Uniforms {
+  var lightPosition: SIMD2<Float>
+  var time: Float
+  var intensity: Float
+  var range: Float
+  var bounceRate: Float
+  var flickerRate: Float
+  var flickerAmplitude: Float
+  var globalIllumination: Float
+  var obstaclePosition: SIMD2<Float>
+  var obstacleSize: SIMD2<Float>
+}
