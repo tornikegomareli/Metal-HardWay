1b8929b168f6881b65387b634d33ea4e8c3d7a40
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-01-07 15:26:18 +0400

    Initial Commit

---
+++ pixel-lighting-metal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pixel-lighting-metal
+//
+//  Created by Tornike Gomareli on 07.01.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pixel-lighting-metal/pixel_lighting_metalApp.swift
@@ -0,0 +1,17 @@
+//
+//  pixel_lighting_metalApp.swift
+//  pixel-lighting-metal
+//
+//  Created by Tornike Gomareli on 07.01.25.
+//
+
+import SwiftUI
+
+@main
+struct pixel_lighting_metalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
