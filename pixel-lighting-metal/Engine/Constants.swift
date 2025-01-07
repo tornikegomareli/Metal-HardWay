69f23ce9d05edf89a3e89b5e7646d5348e6401dc
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-01-07 17:05:27 +0400

    current constants

---
+++ pixel-lighting-metal/Engine/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  pixel-lighting-metal
+//
+//  Created by Tornike Gomareli on 07.01.25.
+//
+
+struct Constants {
+  static let screenWidth: Int = 200
+  static let screenHeight: Int = 200
+  static let scaleFactor: Int = 3
+}
