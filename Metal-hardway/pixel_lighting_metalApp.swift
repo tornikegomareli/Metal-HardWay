15943612c4ff3e47b24e549f4a2c3c0d1e6b7876
Tornike Gomareli <24585160+tornikegomareli@users.noreply.github.com>
2025-01-08 14:53:07 +0400

    wip

---
+++ Metal-hardway/pixel_lighting_metalApp.swift
@@ -0,0 +1,17 @@
+//
+//  pixel_lighting_metalApp.swift
+//  Metal-hardway
+//
+//  Created by Tornike Gomareli on 07.01.25.
+//
+
+import SwiftUI
+
+@main
+struct Metal_Hardway: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
